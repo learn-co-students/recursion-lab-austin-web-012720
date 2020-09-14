f0aa807af6ec351a700f9d8b802e46e42eb0aace
Ben Brower <bbrower.dt@gmail.com>
2020-09-14 18:14:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Code your solution here!
 
+function printString(str) {
+  console.log(str[0])
+  if (str.length > 1){
+    printString(str.substring(1, str.length))
+  } else { 
+    return true
+  }
+}
